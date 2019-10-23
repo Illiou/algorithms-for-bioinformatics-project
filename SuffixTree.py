fd55d585ccea209d4bc52c943f30ad722d554f24
Illiou <manny123@hotmail.de>
2019-10-24 01:56:48 +0200

    added task1, terminal edges, suffix-prefix matching, parent var in nodes

--- SuffixTree.py
+++ SuffixTree.py
@@ -1,71 +1,103 @@
 import json
 
 
+def make_list_or_None(v):
+    if v is None:
+        return None
+    if not isinstance(v, list):
+        return [v]
+    return v
+
+def make_list_or_empty_list(v):
+    if v is None:
+        return []
+    if not isinstance(v, list):
+        return [v]
+    return v
+
+
 class Node:
-    def __init__(self, start=None, end=None, string_id=None, string_pos=None, children=None):
+    def __init__(self, start=None, end=None, string_id=None, string_pos=None, children=None, terminal_edge_ids=None):
         """
         Args:
             start: start position in string string_id, representing string written to the edge going towards this node
             end: end position in string string_id, representing string written to the edge going towards this node
             string_id: list of string id (list pos) to whom start and end correspond to
             string_pos: only in leaf nodes, to string_id corresponding list representing the suffix starting at this position
-            children: list of Nodes being children of this Node
+            children: Node or list of Nodes being children of this Node
+            terminal_edge_ids: string or list of string ids that have terminal edges outgoing from this node
         """
         self.start = start
         self.end = end
-        self.string_id = self.make_list_or_None(string_id)
-        self.string_pos = self.make_list_or_None(string_pos)
+        self.string_id = make_list_or_None(string_id)
+        self.string_pos = make_list_or_None(string_pos)
         self.children = []
         self.add_children(children)
+        self.terminal_edge_ids = make_list_or_empty_list(terminal_edge_ids)
 
-    @staticmethod
-    def make_list_or_None(v):
-        if v is None:
-            return None
-        if not isinstance(v, list):
-            return [v]
-        return v
+        self.parent = None
+        self.path_label_length = 0
 
-    def add_children(self, children):
+    def add_children(self, children: "Node"):  # type annotation just for PyCharm...
         """add child(ren) and return the last one"""
         if children is None:
             return None
-        if isinstance(children, list):
-            self.children.extend(children)
-        else:
-            self.children.append(children)
+        if not isinstance(children, list):
+            children = [children]
+
+        self.children.extend(children)
+        for child in children:
+            child.parent = self
+            child.update_path_label_length()
         return self.children[-1]
 
     def add_string_to_leaf(self, string_id, string_pos):
         self.string_id.append(string_id)
         self.string_pos.append(string_pos)
 
+    def set_start(self, start):
+        self.start = start
+        self.update_path_label_length()
+
+    def set_end(self, end):
+        self.end = end
+        self.update_path_label_length()
+
+    def update_path_label_length(self):
+        self.path_label_length = self.parent.path_label_length + self.end - self.start
+
+
 
 TERMINATION_SYMBOL = "$"
 
 
 class SuffixTree:
-    def __init__(self, strings, construction_method="ukkonen"):
+    def __init__(self, strings=None, construction_method="ukkonen", track_terminal_edges=False):
         """
         Args:
             strings: string or list of strings to be added to the suffix tree
             construction_method: select construction method between "ukkonen" and "naive"
+            track_terminal_edges: keep track of terminal edges for every internal node
         """
-        if not isinstance(strings, list):
-            strings = [strings]
-        self.strings = [s + TERMINATION_SYMBOL for s in strings]
-        self.root = Node()
-        if construction_method == "naive":
-            self._add_string = self._add_string_naive
+        if strings is None:
+            self.strings = []
         else:
-            self._add_string = self._add_string_ukkonen
+            if not isinstance(strings, list):
+                strings = [strings]
+            self.strings = [s + TERMINATION_SYMBOL for s in strings]
+        self.root = Node()
+        self._add_string = self._add_string_naive if construction_method == "naive" else self._add_string_ukkonen
+        self.track_terminal_edges = track_terminal_edges
 
         self._construct()
 
     def add_string(self, string):
+        """adds single string to SuffixTree and returns it's string_id"""
         string = string + TERMINATION_SYMBOL
         self.strings.append(string)
-        self._add_string(string, len(self.strings) - 1)
+        string_id = len(self.strings) - 1
+        self._add_string(string, string_id)
+        return string_id
 
     def _construct(self):
         for string_id, string in enumerate(self.strings):
@@ -74,13 +106,13 @@ class SuffixTree:
     def _add_string_naive(self, string, string_id):
         for i in range(len(string)):  # add suffix i..m
             suffix = string[i:]
-            parent = self.root
+            current_node = self.root
             suffix_pos = 0
             node_found = False
             # find node to add leaf node on
             while not node_found:
                 # find child node with matching label
-                for child_id, child in enumerate(parent.children):
+                for child_id, child in enumerate(current_node.children):
                     child_string = self.strings[child.string_id[0]]
                     # check first label character of current node
                     if suffix[suffix_pos] == child_string[child.start]:
@@ -90,38 +122,73 @@ class SuffixTree:
                             # splitting point?
                             if suffix[suffix_pos] != child_string[label_pos]:
                                 # add splitting node
-                                split_node = parent.add_children(Node(child.start, label_pos, child.string_id))
-                                split_node.add_children(parent.children.pop(child_id))
-                                child.start = label_pos
+                                split_node = current_node.add_children(Node(child.start, label_pos, child.string_id))
+                                split_node.add_children(current_node.children.pop(child_id))
+                                child.set_start(label_pos)
                                 # to add leaf node
-                                parent = split_node
+                                current_node = split_node
                                 node_found = True
                                 break
                             suffix_pos += 1
                         # matched until next node, repeat process
                         else:
-                            parent = child
+                            current_node = child
                         break
                 # no child matched rest of suffix
                 else:
                     node_found = True
-            # add leaf node or add string_id to existing node
+            # add string_id to existing leaf node...
             if suffix_pos == len(suffix):
-                parent.add_string_to_leaf(string_id, i)
-            else:
-                parent.add_children(Node(i + suffix_pos, len(string), string_id, i))
+                current_node.add_string_to_leaf(string_id, i)
+                if self.track_terminal_edges:
+                    current_node.parent.terminal_edge_ids.append(string_id)
+            else:  # ...or add new leaf node
+                current_node.add_children(Node(i + suffix_pos, len(string), string_id, i))
+                if self.track_terminal_edges and suffix_pos == len(suffix) - 1:
+                    current_node.terminal_edge_ids.append(string_id)
 
     def _add_string_ukkonen(self, string, string_id):
-        raise NotImplementedError()
+        raise NotImplementedError("Ukkonen not yet implemented, pass construction_method=\"naive\" to SuffixTree")
+
+    def find_suffix_matches_for_prefix(self, prefix_string_id):
+        current_node = self.root
+        prefix_pos = 0
+        string_stacks = {}
+        while len(current_node.children) > 0:
+            # add current terminal edge string ids to stack
+            for string_id in current_node.terminal_edge_ids:
+                string_stacks.setdefault(string_id, [])
+                string_stacks[string_id].append(current_node)
+            # look for child with prefix matching label
+            for child in current_node.children:
+                child_string = self.strings[child.string_id[0]]
+                if self.strings[prefix_string_id][prefix_pos] == child_string[child.start]:
+                    assert self.strings[prefix_string_id][prefix_pos:].find(child_string[child.start:]) == 0
+                    prefix_pos += child.end - child.start
+                    current_node = child
+                    break
+            else:
+                raise AssertionError("didn't find prefix in tree")
+        # record how far each string matched into the prefix string
+        prefix_match_pos = {string_id: stack[-1].path_label_length for string_id, stack in string_stacks.items()}
+        return prefix_match_pos
 
     def __repr__(self):
-        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+        # needed to remove circularity
+        def remove_parent(o):
+            d = o.__dict__
+            if isinstance(o, Node):
+                del d["parent"]
+            return d
+        return json.dumps(self, default=lambda o: remove_parent(o), indent=4)
 
     def render_children(self, node, root_label=None):
         if root_label is not None:
             label = root_label
         else:
             label = f"|-{self.strings[node.string_id[0]][node.start:node.end]}"
+            if self.track_terminal_edges and node.terminal_edge_ids != []:
+                label += f" ({', '.join(str(n) for n in node.terminal_edge_ids)})"
         if len(node.children) > 0:
             all_lines = []
             for child in node.children:
@@ -139,7 +206,7 @@ class SuffixTree:
             return all_lines
         else:
             strings = list(zip(node.string_id, node.string_pos))
-            return [f"{label}\t\t{strings}"]
+            return [f"{label}\t\t\t{strings}"]
 
     def __str__(self):
         return "\n ".join(self.render_children(self.root, root_label="()"))
@@ -147,6 +214,6 @@ class SuffixTree:
 
 if __name__ == '__main__':
     test_string = ["gctgca", "tgc", "gct"]
-    tree = SuffixTree(test_string, construction_method="naive")
+    tree = SuffixTree(test_string, construction_method="naive", track_terminal_edges=True)
     print(repr(tree))
     print(tree)
--- task1.py
+++ task1.py
@@ -1,1 +1,46 @@
-# to be added
+from SuffixTree import SuffixTree
+import time
+
+def current_milli_time():
+    return round(time.perf_counter() * 1000)
+
+adapter = "TGGAATTCTCGGGTGCCAAGGAACTCCAGTCACACAGTGATCTCGTATGCCGTCTTCTGCTTG"
+dataset = "s_3_sequence_1M.txt"
+dataset_path = f"datasets/{dataset}"
+
+number_of_lines = 1000
+
+suffix_tree = SuffixTree(adapter, construction_method="naive", track_terminal_edges=True)
+adapter_string_id = 0
+
+start_time = current_milli_time()
+with open(dataset_path, "r") as file:
+    for line_num, line in enumerate(file):
+        if line_num >= number_of_lines:
+            break
+        suffix_tree.add_string(line.strip())
+
+end_time = current_milli_time()
+print(f"Time needed: {end_time - start_time} ms")
+
+# print(repr(suffix_tree))
+# print(suffix_tree)
+
+adapter_match_lengths = suffix_tree.find_suffix_matches_for_prefix(adapter_string_id)
+print(adapter_match_lengths)
+
+matched_suffixes = []
+for string_id, match_length in adapter_match_lengths.items():
+    matched_suffixes.append(adapter[:match_length])
+    if matched_suffixes[-1] != suffix_tree.strings[string_id][-match_length - 1:-1]:
+        print(f"Something went wrong at string {string_id} with match length {match_length}")
+        print(f"Adapter part: {matched_suffixes[-1]}   string part: {suffix_tree.strings[string_id][-match_length - 1:-1]}")
+print(matched_suffixes)
+
+# TODO: fix this bug:
+# Something went wrong at string 421 with match length 6
+# Adapter part: TGGAAT   string part: TGTTGA
+
+# TODO: remove adapter sequence from result and make sure indices are correct (dict -> list)
+# import numpy as np
+# length_distribution = np.histogram(adapter_match_lengths.keys(), bins=len(adapter_match_lengths))
