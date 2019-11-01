e4f6cbf1c8d58db5e380309190e675056ba88083
Illiou <manny123@hotmail.de>
2019-11-01 14:24:29 +0100

    added task3 most common suffixes of unique sequences

--- task3.py
+++ task3.py
@@ -28,6 +28,7 @@ time_param = f"_{get_current_time_for_filename()}"
 
 outputs_path = "outputs/"
 unique_sequences_output_path = f"{outputs_path}task3_unique_sequences{lines_param}{time_param}.json"
+unique_sequences_most_common_suffixes_output_path = f"{outputs_path}task3_unique_sequences_most_common_suffixes{lines_param}{time_param}.json"
 most_common_suffixes_output_path = f"{outputs_path}task3_most_common_suffixes{lines_param}{time_param}.json"
 adapter_match_lengths_output_path = f"{outputs_path}task3_adapter_match_lengths{lines_param}{time_param}.json"
 
@@ -122,3 +123,29 @@ if save_graphs:
     curr_ax.bar(np.arange(len(remaining_lengths_distribution)), remaining_lengths_distribution)
     curr_ax.set(title="Task 3: Remaining Sequence Length Distribution", xlabel="Remaining Sequence Length", ylabel="Occurrence Probability")
     curr_fig.savefig(remaining_lengths_distribution_graph_path)
+
+
+# ---------------- Find most common suffixes of unique sequences ----------------
+del suffix_tree
+
+suffix_tree = SuffixTree()
+
+start_time = current_milli_time()
+for _, unique_sequence in unique_sequences:
+    suffix_tree.add_string(unique_sequence)
+end_time = current_milli_time()
+
+print(f"\nTime needed to compute unique sequence Suffix Tree: {end_time - start_time} ms")
+
+start_time = current_milli_time()
+unique_sequences_most_common_suffixes = suffix_tree.find_most_common_suffixes()
+end_time = current_milli_time()
+
+print(f"\nTime needed to find most common suffixes in unique sequence Suffix Tree: {end_time - start_time} ms")
+
+if save_outputs:
+    with open(unique_sequences_most_common_suffixes_output_path, "w") as file:
+        json.dump([(count, length, suffix_tree.strings[node.string_id[0]][-length:-1])
+                   for count, length, node in unique_sequences_most_common_suffixes[:1000]], file)
+else:
+    print(unique_sequences_most_common_suffixes[:1000])
